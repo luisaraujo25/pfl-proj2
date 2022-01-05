806991417d267aada888dfef8cf2368d902b4f6d
Luísa Araújo <up201904996@fe.up.pt>
2022-01-05 09:17:23 +0000

    clearScreen

--- display.pl
+++ display.pl
@@ -1,7 +1,10 @@
+%:- include('file.pl').
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
 :- use_module(library(sets)).
 
-displayBoard(B) :-
-	write('\33\[2J'),
+clearScreen :- write('\33\[2J').
+
+displayBoard :-
+	clearScreen,
 	write('FIRST SENTENCE').
