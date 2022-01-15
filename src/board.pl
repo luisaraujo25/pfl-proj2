193fc112ab59a8569269521f07b0c50b7cfca5af
JoaoRodrigo99 <up201705110@fe.up.pt>
2022-01-15 16:40:17 +0000

    Board setup

--- src/board.pl
+++ src/board.pl
@@ -2,9 +2,33 @@
 boardSize(9).
 
 %Pieces
-piece(white, 'W').
-piece(black, 'B').
-piece(none, ' ').
+% piece(white, 'W').
+% piece(black, 'B').
+% piece(none, ' ').
+% initial(piece(white, X, 1)) :-
+%     between(1, 8, X).
+
+initial(piece(white, 1, 1)).
+initial(piece(white, 2, 1)).
+initial(piece(white, 3, 1)).
+initial(piece(white, 4, 1)).
+initial(piece(white, 5, 1)).
+initial(piece(white, 6, 1)).
+initial(piece(white, 7, 1)).
+initial(piece(white, 8, 1)).
+initial(piece(white, 9, 1)).
+
+
+% initial(piece(white, X, 2)) :-
+%     between(1, 9, X).
+% initial(piece(black, X, 9)) :-
+%     between(1, 9, X).
+% initial(piece(black, X, 8)) :-
+%     between(1, 9, X).
+
+initial_board(Board) :-
+    findall(Piece, initial(Piece), Board).
+
 
 %addPiece
 
@@ -33,3 +57,4 @@ getPiece(_, Piece, 0, _).
 getPiece(Board, Piece, X, Y) :-
     N is X-1,
     getPiece(Board,Piece,N,Y).
+
